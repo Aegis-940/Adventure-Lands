608f8aa8a13a2ddb8bdfe2c05d8ebcc322fa87df
Aegis-940 <jay.sumner996@gmail.com>
2025-07-24 20:06:09 +1000

    Update Merchant.js

--- Characters/Merchant.js
+++ Characters/Merchant.js
@@ -1,27 +1,26 @@
-
 // -------------------------------------------------------------------- //
 // TASK QUEUE
 // -------------------------------------------------------------------- //
 
-function enqueueMerchantTask(taskFn) {
-	merchantTaskQueue.push(taskFn);
-	processMerchantQueue();
+function enqueue_merchant_task(taskFn) {
+	MERCHANT_TASK_QUEUE.push(taskFn);
+	process_merchant_queue();
 }
 
-async function processMerchantQueue() {
-	if (merchantBusy || merchantTaskQueue.length === 0) return;
+async function process_merchant_queue() {
+	if (merchant_busy || MERCHANT_TASK_QUEUE.length === 0) return;
 
-	merchantBusy = true;
+	merchant_busy = true;
 
-	const task = merchantTaskQueue.shift();
+	const task = MERCHANT_TASK_QUEUE.shift();
 	try {
 		await task(); // Run the task
 	} catch (e) {
 		console.error("❌ Merchant task error:", e);
 	}
 	
-	merchantBusy = false;
-	processMerchantQueue(); // Continue with the next task
+	merchant_busy = false;
+	process_merchant_queue(); // Continue with the next task
 }
 
 // -------------------------------------------------------------------- //
@@ -34,8 +33,8 @@ removeAllFloatingStatsWindows();
 
 createMapMovementWindow([
   { id: "SellBank", label: "Sell / Bank", onClick: () => sell_and_bank() },
-  { id: "CollectLoot", label: "Take Loot", onClick: () => checkRemoteInventories(),  },
-  { id: "GoFish", label: "Go Fish", onClick: () => goFish() },
+  { id: "CollectLoot", label: "Take Loot", onClick: () => check_remote_inventories()  },
+  { id: "GoFish", label: "Go Fish", onClick: () => go_fish() },
   { id: "custom4", label: "Custom 4", onClick: () => null },
   { id: "custom5", label: "Custom 5", onClick: () => null },
   { id: "custom6", label: "Custom 6", onClick: () => null }
@@ -53,23 +52,23 @@ setInterval(function () {
 	if (now - last_inventory_check >= INVENTORY_CHECK_INTERVAL) {
 		last_inventory_check = now;
 		game_log("*** Checking for loot ***")
-		checkRemoteInventories();
+		check_remote_inventories();
 	}
 	
 	// Detect death and record time
-	if (character.rip && lastDeathTime === 0) {
-		lastDeathTime = Date.now();
+	if (character.rip && last_death_time === 0) {
+		last_death_time = Date.now();
 	}
 
 	// Revive after 30 seconds
-	if (character.rip && Date.now() - lastDeathTime >= 30000) {
+	if (character.rip && Date.now() - last_death_time >= 30000) {
 		respawn();
-		lastDeathTime = 0;
+		last_death_time = 0;
 	}
 
 	// Reset if revived manually
-	if (!character.rip && lastDeathTime !== 0) {
-		lastDeathTime = 0;
+	if (!character.rip && last_death_time !== 0) {
+		last_death_time = 0;
 	}
 
     pots();
