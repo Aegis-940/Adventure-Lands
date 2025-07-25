f0a96f95d179e0ede41889c5d8c492cb65293ca1
Aegis-940 <jay.sumner996@gmail.com>
2025-07-25 23:45:03 +1000

    Update Ranger Functions.js

--- Character Functions/Ranger Functions.js
+++ Character Functions/Ranger Functions.js
@@ -1,42 +1,4 @@
 
-const locations = {
-    bat: [{ x: 1200, y: -782 }],
-    bbpompom: [{ x: -82, y: -949 }],
-    bigbird: [{ x: 1300, y: -69 }],
-    bluefairy: [{ x: -357, y: -675 }],
-    boar: [{ x: 19, y: -1109 }],
-    bscorpion: [{ x: -616, y: -1279 }],
-    cgoo: [{ x: -221, y: -274 }],
-    crab: [{ x: -11840, y: -37 }],
-    dryad: [{ x: 403, y: -347 }],
-    ent: [{ x: -420, y: -1960 }],
-    fireroamer: [{ x: 222, y: -827 }],
-    ghost: [{ x: -405, y: -1642 }],
-    gscorpion: [{ x: 390, y: -1422 }],
-    iceroamer: [{ x: 823, y: -45 }],
-    mechagnome: [{ x: 0, y: 0 }],
-    mole: [{ x: 14, y: -1072 }],
-    mummy: [{ x: 256, y: -1417 }],
-    odino: [{ x: -52, y: 756 }],
-    oneeye: [{ x: -270, y: 160 }],
-    pinkgoblin: [{ x: 366, y: 377 }],
-    poisio: [{ x: -121, y: 1360 }],
-    prat: [{ x: -296, y: 558 }], //[{ x: 6, y: 430 }]
-    pppompom: [{ x: 292, y: -189 }],
-    plantoid: [{ x: -780, y: -387 }], // [{ x: -840, y: -340 }]
-    rat: [{ x: -223, y: -313 }],
-    scorpion: [{ x: -495, y: 685 }],
-    stoneworm: [{ x: 830, y: 7 }],
-    spider: [{ x: 1247, y: -91 }],
-    squig: [{ x: -1175, y: 422 }],
-    targetron: [{ x: -544, y: -275 }],
-    wolf: [{ x: 433, y: -2745 }],
-    wolfie: [{ x: 113, y: -2014 }],
-    xscorpion: [{ x: -495, y: 685 }]
-};
-
-const home = 'targetron';
-
 // --------------------------------------------------------------------------------------------------------------------------------- //
 // ATTACK LOOP
 // --------------------------------------------------------------------------------------------------------------------------------- //
@@ -44,7 +6,7 @@ const home = 'targetron';
 let lastSwitchTime = 0, state = "attacking";
 const switchCooldown = 750;
 const rangeThreshold = 45;
-const X = locations[home][0].x, Y = locations[home][0].y;
+const X = character.x, Y = character.y;
 let lastEquippedSet = null;
 
 async function attack_loop() {
