f9237c4c48f6680affe260c459aae5f36f179bdc
Aegis-940 <jay.sumner996@gmail.com>
2025-07-27 09:57:05 +1000

    Update Ranger Functions.js

--- Character Functions/Ranger Functions.js
+++ Character Functions/Ranger Functions.js
@@ -187,7 +187,7 @@ async function attack_loop() {
             sortedByHP.push(e);
         }
     }
-    sortedByHP.sort((a, b) => b.hp - a.hp);
+    sortedByHP.sort((a, b) => a.hp - b.hp);
 
     const inRange = [], outOfRange = [];
     for (const mob of sortedByHP) {
