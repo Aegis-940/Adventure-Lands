0634fb1e3553b0d22f58925a1487f219f85bf3d3
Aegis-940 <jay.sumner996@gmail.com>
2025-07-26 20:19:55 +1000

    Update Healer.js

--- Characters/Healer.js
+++ Characters/Healer.js
@@ -12,17 +12,15 @@ remove_all_floating_stats_windows();
 remove_all_floating_buttons();
 
 create_map_movement_window([
-	{ id: "SendToMerchant", label: "Deposit", onClick: () => send_to_merchant() },
-	{ id: "custom2", label: "Custom 2", onClick: () => null },
-	{ id: "custom3", label: "Custom 3", onClick: () => null },
-	{ id: "custom4", label: "Custom 4", onClick: () => null },
-	{ id: "custom5", label: "Custom 5", onClick: () => null },
-	{ id: "custom6", label: "Custom 6", onClick: () => null }
+  { id: "SendToMerchant", label: "Deposit", onClick: () => send_to_merchant() },
+  { id: "custom2", label: "Custom 2", onClick: () => null },
+  { id: "custom3", label: "Custom 3", onClick: () => null },
+  { id: "custom4", label: "Custom 4", onClick: () => null },
+  { id: "custom5", label: "Custom 5", onClick: () => null },
+  { id: "custom6", label: "Custom 6", onClick: () => null }
 ]);
 
 toggle_combat();
-toggle_tank_role();
-toggle_follow_tank();
 toggle_free_move();
 hide_skills_ui();
 // toggle_stats_window();
