7cd48d0a49f2ebc56f39dcd6fe3847fc147e5f63
Aegis-940 <jay.sumner996@gmail.com>
2025-07-26 20:17:18 +1000

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -197,7 +197,7 @@ function toggle_free_move() {
     );
   }, {
     top:     "2.05vh",
-    right:   "582px",
+    right:   "583px",
     minWidth:"56px",
     height:  "56px",
     fontSize:"24px",
