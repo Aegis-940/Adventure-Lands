84e32d8740b618ac24c1f75f535d936bb4fe0f1f
Aegis-940 <jay.sumner996@gmail.com>
2025-07-27 10:48:10 +1000

    Update Ranger Functions.js

--- Character Functions/Ranger Functions.js
+++ Character Functions/Ranger Functions.js
@@ -188,7 +188,7 @@ async function attack_loop() {
 
     const SORTED_BY_HP = [];
     for (const e of entities) {
-        if (e.type === "monster" && (e.target === MONSTER_TYPES[0] || e.target === MONSTER_TYPES[1])) {
+        if (e.type === "monster" && (e.mtype === MONSTER_TYPES[0] || e.mtype === MONSTER_TYPES[1])) {
             SORTED_BY_HP.push(e);
         }
     }
