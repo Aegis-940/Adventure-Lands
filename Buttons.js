45fd52d35787d8b7b1caaf6b895932d4b7dfe684
Aegis-940 <jay.sumner996@gmail.com>
2025-07-27 19:11:31 +1000

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -308,24 +308,34 @@ function toggle_inventory_check() {
 // -------------------------------------------------------------------- //
 
 function toggle_maintain_position() {
-	create_floating_button("toggle_maintain_position", "🌐", async () => {
-		toggle_radius_lock();
+	if (!window.top.document.getElementById("toggle_maintain_position")) {
+		create_floating_button("toggle_maintain_position", "🌐", async () => {
+			toggle_radius_lock();
 
-		const btn = window.top.document.getElementById("toggle_maintain_position");
-		btn.innerText = radius_lock_enabled  ? "🌐" : "🎯";
+			const btn = window.top.document.getElementById("toggle_maintain_position");
+			btn.innerText = radius_lock_enabled ? "🌐" : "🎯";
 
-		if (radius_lock_enabled) {
-			game_log("🌐 Maintain Position ENABLED");
-		} else {
-			game_log("🎯 Maintain Position DISABLED");
-		}
-	}, {
-		top: "2.1vh",
-		right: "645px",
-		minWidth: "56px",
-		height: "56px",
-		fontSize: "24px",
-		border: "4px solid #888",
-		title: "Toggle Maintain Position"
-	});
+			if (radius_lock_enabled) {
+				game_log("🌐 Maintain Position ENABLED");
+			} else {
+				game_log("🎯 Maintain Position DISABLED");
+			}
+		}, {
+			top: "2.1vh",
+			right: "645px",
+			minWidth: "56px",
+			height: "56px",
+			fontSize: "24px",
+			border: "4px solid #888",
+			title: "Toggle Maintain Position"
+		});
+
+		setTimeout(() => {
+			const btn = window.top.document.getElementById("toggle_maintain_position");
+			if (btn) {
+				btn.style.borderRadius = "0px";
+			}
+		}, 0);
+	}
 }
+
