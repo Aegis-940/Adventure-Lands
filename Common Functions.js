3e18b8c6b44af8bbaf5c19b60379e295b773a31c
Aegis-940 <jay.sumner996@gmail.com>
2025-08-01 08:49:41 +1000

    Update Common Functions.js

--- Common Functions.js
+++ Common Functions.js
@@ -487,7 +487,7 @@ async function follow_priest_loop() {
     request_priest_location();
 game_log("Check 3");
     const priest_location = location_responses["Myras"];
-	game_logpriest_location);
+	game_log(priest_location);
     if (!priest_location) return;
 game_log("Check 4");
     const { map, x, y } = priest_location;
