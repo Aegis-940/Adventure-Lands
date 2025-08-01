9f11f86eee8fdc026d5661afb27573e044c44c83
Aegis-940 <jay.sumner996@gmail.com>
2025-08-01 20:34:24 +1000

    Update Common Functions.js

--- Common Functions.js
+++ Common Functions.js
@@ -27,7 +27,7 @@ const PARTY_MEMBERS               = ["Riva", "Myras", "Riff"];
 const FOLLOW_DISTANCE             = 150;
 
 const MONSTER_TYPES               = ["goo", "bee", "crab", "snake", "osnake", "bat", "goldenbat", "croc", "arcticbee", "spider", "cgoo", "stoneworm", "jr", "minimush", 
-                                     "rat", "bbpompom", "tortoise", "crabx", "porcupine", "armadillo", "squig", "ghost"];
+                                     "rat", "bbpompom", "tortoise", "crabx", "porcupine", "armadillo", "squig", "ghost", "phoenix"];
 const MERCHANT_NAME               = "Riff";
 
 let last_death_time               = 0;
