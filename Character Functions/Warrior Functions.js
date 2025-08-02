58e6e6246bff1314848c5a18b90dcc64e55fd49c
Aegis-940 <jay.sumner996@gmail.com>
2025-08-02 12:03:59 +0000

    1

--- Character Functions/Warrior Functions.js
+++ Character Functions/Warrior Functions.js
@@ -347,7 +347,7 @@ function equip_set(setName) {
 
 function handle_weapon_swap() {
 	const now = performance.now();
-	if (now - eTime <= 50) return;
+	if (now - eTime <= 200) return;
 
     equip_set("single");
     eTime = now;
