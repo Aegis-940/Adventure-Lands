a9e6c4efb21fb83a339d4001fe82d18d2e37c495
Aegis-940 <jay.sumner996@gmail.com>
2025-08-02 12:04:18 +1000

    Update Warrior Functions.js

--- Character Functions/Warrior Functions.js
+++ Character Functions/Warrior Functions.js
@@ -306,7 +306,7 @@ const equipment_sets = {
     ],
     single: [
         { itemName: "fireblade", slot: "mainhand", level: 7, l: "l" },
-        { itemName: "fireblade", slot: "offhand", level: 7, l: "u" },
+        { itemName: "ololipop", slot: "offhand", level: 5, l: "u" },
     ],
     cleave: [
         { itemName: "bataxe", slot: "mainhand", level: 5, l: "s" }
