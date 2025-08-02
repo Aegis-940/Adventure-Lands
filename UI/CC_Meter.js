30fd7f69f62b2821a5b6bb03d9fa6ad2e1ffeee0
Aegis-940 <jay.sumner996@gmail.com>
2025-08-02 16:56:03 +1000

    Update CC_Meter.js

--- UI/CC_Meter.js
+++ UI/CC_Meter.js
@@ -10,7 +10,7 @@ const cc_meter = () => {
 	const brc = $('#bottomrightcorner');
 	brc.find('#ccmeter').remove();
 
-	const ccContainer = $('<div id="ccmeter"></div>').css({
+	const cc_container = $('<div id="ccmeter"></div>').css({
 		width:        '100%',
 		marginTop:    '4px',
 		marginBottom: '-4px',
@@ -20,7 +20,7 @@ const cc_meter = () => {
 		display:      'table',
 	});
 
-	const ccBar = $('<div id="ccbar"></div>').css({
+	const cc_bar = $('<div id="ccbar"></div>').css({
 		display:       'table-cell',
 		verticalAlign: 'middle',
 		width:         '100%',
@@ -30,42 +30,44 @@ const cc_meter = () => {
 		position:      'relative',
 	});
 
-	const ccFill = $('<div id="ccfill"></div>').css({
-		position: 'absolute',
-		top:      0,
-		left:     0,
-		height:   '100%',
-		width:    '0%',
+	const cc_fill = $('<div id="ccfill"></div>').css({
+		position:   'absolute',
+		top:        0,
+		left:       0,
+		height:     '100%',
+		width:      '0%',
 		background: 'linear-gradient(to right, #1e90ff, #4169e1)',
 	});
 
-	const ccText = $('<div id="cctext"></div>').css({
-		position:     'absolute',
-		width:        '100%',
-		height:       '100%',
-		lineHeight:   '28px',
-		fontWeight:   'bold',
-		color:        '#FFFFFF',
-		textShadow:   '1px 1px 2px black',
+	const cc_text = $('<div id="cctext"></div>').css({
+		position:       'absolute',
+		top:            0,
+		left:           0,
+		width:          '100%',
+		height:         '100%',
+		display:        'flex',
+		alignItems:     'center',
+		justifyContent: 'center',
+		fontWeight:     'bold',
+		color:          '#FFFFFF',
+		textShadow:     '1px 1px 2px black',
+		pointerEvents:  'none',
 	});
 
-	ccBar.append(ccFill).append(ccText);
-	ccContainer.append(ccBar);
-	brc.children().first().after(ccContainer);
+	cc_bar.append(cc_fill).append(cc_text);
+	cc_container.append(cc_bar);
+	brc.children().first().after(cc_container);
 };
 
 // Update the cc meter display
 const update_cc_display = () => {
 	const $ = parent.$;
-	const currentCc = Math.min(character.cc, MAX_CC);
-	const percent = (currentCc / MAX_CC * 100).toFixed(1);
+	const current_cc = Math.min(character.cc, MAX_CC);
+	const percent = Math.floor((current_cc / MAX_CC) * 100);
 
 	$('#ccfill').css('width', `${percent}%`);
-	$('#cctext').text(`CC: ${currentCc}/${MAX_CC}`);
+	$('#cctext').text(`CC: ${Math.floor(current_cc)}/${MAX_CC}`);
 };
 
 // Refresh the CC bar 10 times per second
 setInterval(update_cc_display, 100);
-
-// Kick things off
-cc_meter();
