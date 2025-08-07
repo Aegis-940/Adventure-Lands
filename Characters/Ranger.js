688aa4a0fec0859c55634d0309a679fa11ff332f
Aegis-940 <jay.sumner996@gmail.com>
2025-08-07 23:38:33 +0000

    1

--- Characters/Ranger.js
+++ Characters/Ranger.js
@@ -21,6 +21,7 @@ toggle_follow_priest_button();
 hide_skills_ui();
 
 potions_loop();
+loot_loop();
 //panic_button_loop();
 
 // --------------------------------------------------------------------------------------------------------------------------------- //
@@ -40,7 +41,6 @@ setInterval(() => {
 
 	// === Core utility loops ===
 	//pots();
-	loot();
 	party_manager();
 	
 	if (!attack_mode || character.rip || is_moving(character)) return;
