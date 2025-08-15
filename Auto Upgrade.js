ce2a8281633ecb4d5fb213e29fe1dd368f13d64e
Aegis-940 <jay.sumner996@gmail.com>
2025-08-15 11:28:59 +0000

    1

--- Auto Upgrade.js
+++ Auto Upgrade.js
@@ -12,6 +12,7 @@ const upgradeProfile = {
   firestaff:   { scroll0_until: 0, scroll1_until: 4, scroll2_until: 7, primling_from: 6, max_level: 6 },
   hbow:        { scroll0_until: 3, scroll1_until: 6, scroll2_until: 8, primling_from: 7, max_level: 7 },
   wingedboots: { scroll0_until: 2, scroll1_until: 6, scroll2_until: 8, primling_from: 7, max_level: 6 },
+  basher:      { scroll0_until: 0, scroll1_until: 3, scroll2_until: 6, primling_from: 5, max_level: 3 },
   // Add more items as needed
 };
 
