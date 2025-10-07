e3c802d6aeae6a5f0ceebabf8091714ffa9cb95d
Aegis-940 <jay.sumner996@gmail.com>
2025-10-07 01:47:15 +0000

    1

--- Character Functions/Ranger Functions.js
+++ Character Functions/Ranger Functions.js
@@ -159,7 +159,7 @@ async function attack_loop() {
     const X = character.x, Y = character.y;
 
     // Boss detection logic
-    const boss_alive = BOSSES.some(name => parent.S[name].live);
+    const boss_alive = BOSSES.some(name => parent.S[name] && parent.S[name].live);
     if (boss_alive) {
         stop_attack_loop();
         boss_loop();
