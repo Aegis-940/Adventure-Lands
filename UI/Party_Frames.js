200c5ac4e8e78b6ef7830a03f995ec23f99131b9
Aegis-940 <jay.sumner996@gmail.com>
2025-10-10 06:17:48 +0000

    1

--- UI/Party_Frames.js
+++ UI/Party_Frames.js
@@ -5,8 +5,8 @@ if (parent.party_style_prepared) {
 let css = `
     .party-container {
         position: absolute;
-        top: -5px;
-        left: -500px;
+        top: -4px;
+        left: -450px;
         width: auto;
         height: auto;
         font-family: 'pixel';
