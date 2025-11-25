76d56135e626dbc0a7d4bd64e9b997bdc9f7a3cc
Aegis-940 <jay.sumner996@gmail.com>
2025-11-25 03:19:27 +0000

    1

--- Character Functions/Warrior Functions.js
+++ Character Functions/Warrior Functions.js
@@ -602,11 +602,11 @@ async function explosion_set() {
     const mainhand = character.slots.mainhand;
     const offhand = character.slots.offhand;
     const needs_main = !mainhand || mainhand.name !== "fireblade" || mainhand.level !== 8 || mainhand.l !== "l";
-    const needs_off = !offhand || offhand.name !== "ololipop" || offhand.level !== 8 || offhand.l !== "l";
+    const needs_off = !offhand || offhand.name !== "ololipop" || offhand.level !== 9 || offhand.l !== "l";
     if (needs_main || needs_off) {
         batch_equip([
             { itemName: "fireblade", slot: "mainhand", level: 8, l: "l" },
-            { itemName: "ololipop", slot: "offhand", level: 8, l: "l" }
+            { itemName: "ololipop", slot: "offhand", level: 9, l: "l" }
         ]);
     }
     weapon_set_equipped = "explosion";
