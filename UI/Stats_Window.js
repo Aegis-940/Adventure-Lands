ec86b0b98d6e68f3fbbba6062e21d4ae2eed77d7
Aegis-940 <jay.sumner996@gmail.com>
2025-12-15 09:00:54 +0000

    1

--- UI/Stats_Window.js
+++ UI/Stats_Window.js
@@ -79,7 +79,7 @@ function add_gold_graph(doc, content) {
             ctx.stroke();
 
             // Y-axis min/max labels
-            ctx.font = "16px pixel, monospace";
+            ctx.font = "24px pixel, monospace";
             ctx.fillStyle = "#fff";
             ctx.textAlign = "left";
             ctx.textBaseline = "middle";
@@ -94,7 +94,7 @@ function add_gold_graph(doc, content) {
         if (typeof calculateAverageGold === "function") {
             goldPerHour = calculateAverageGold();
         }
-        ctx.font = "16px pixel, monospace";
+        ctx.font = "24px pixel, monospace";
         ctx.fillStyle = "#0ff";
         ctx.textAlign = "right";
         ctx.textBaseline = "bottom";
