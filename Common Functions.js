853681e0cc9a076d4106f3e77d48e0c53c38088e
Aegis-940 <jay.sumner996@gmail.com>
2026-03-04 10:04:28 +0000

    1

--- Common Functions.js
+++ Common Functions.js
@@ -1731,3 +1731,5 @@ function moveElementUpByPx(elementId, pixels) {
 moveElementUpByPx("bottomleftcorner2", 370);
 moveElementUpByPx("chatwparty", 370);
 moveElementUpByPx("chatinput", 370);
+
+parent.$('#bottomleftcorner').show();
