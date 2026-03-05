a242f44b922f828ceff205623815b7c01b59c094
Aegis-940 <jay.sumner996@gmail.com>
2026-03-05 23:14:07 +0000

    1

--- Common Functions.js
+++ Common Functions.js
@@ -1760,7 +1760,7 @@ parent.$('#bottomleftcorner').show();
 const PRIM_FARM_LOC = { map: "desertland", x: -408, y: -1266 };
 const PRIM_FARM_LOC_HEALER = { map: "desertland", x: -408, y: -1146 };
 const PRIM_FARM_RADIUS = 100;
-const SAFETY_DISTANCE = 100;
+const SAFETY_DISTANCE = 150;
 
 function get_bscorpion_info() {
     let nearest = null;
