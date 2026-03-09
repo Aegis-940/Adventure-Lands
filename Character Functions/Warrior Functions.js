84dc4d519fa8892bd9987d29a4895bfa599e67c8
Aegis-940 <jay.sumner996@gmail.com>
2026-03-09 08:23:19 +0000

    1

--- Character Functions/Warrior Functions.js
+++ Character Functions/Warrior Functions.js
@@ -31,7 +31,7 @@ async function sugar_rush_check(target) {
 
     if (character.s.sugarrush === undefined) {
         equip_batch([{ num: 6, slot: "mainhand" }, { num: 7, slot: "offhand" }]);
-        await delay(100);
+        await delay(200);
         equip_batch([{ num: 6, slot: "mainhand" }, { num: 7, slot: "offhand" }]);
         await delay(200);
         if (character.s.sugarrush !== undefined) {
