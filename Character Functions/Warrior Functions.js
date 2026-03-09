445bfbc0c6274a31b4666eb470b284a5a600a223
Aegis-940 <jay.sumner996@gmail.com>
2026-03-09 02:42:21 +0000

    1

--- Character Functions/Warrior Functions.js
+++ Character Functions/Warrior Functions.js
@@ -91,6 +91,7 @@ async function attack_loop() {
                 }
                 await delay(201)
                 delayMs = ms_to_next_skill('attack') + character.ping + 50;
+                log(delayMs)
                 await delay(delayMs);
                 continue;
             } catch (e) {
