9c11aef472a66e9b3bbb3fc831b753fab8096f8e
Aegis-940 <jay.sumner996@gmail.com>
2026-03-09 04:24:52 +0000

    1

--- Common Functions.js
+++ Common Functions.js
@@ -1916,8 +1916,8 @@ async function move_distance_from_bscorpion() {
 
     if (!nearest) return false;
 
-    const desired = 38;
-    const tolerance = 1;
+    const desired = 37;
+    const tolerance = 0.75;
     if (Math.abs(minDist - desired) > tolerance) {
         // Only move if not already moving or target is far from current move target
         if (!character.moving || Math.hypot(character.x - nearest.x, character.y - nearest.y) > tolerance) {
