c161e8d1e3f79615a436d700591e3f13d7a76fdc
Aegis-940 <jay.sumner996@gmail.com>
2026-03-09 02:55:54 +0000

    1

--- Common Functions.js
+++ Common Functions.js
@@ -1855,7 +1855,7 @@ async function move_distance_from_bscorpion() {
         }
     }
     if (!nearest) return false; // No bscorpion found
-    const desired = character.range - 2;
+    const desired = 38;
     const angle = Math.atan2(character.y - nearest.y, character.x - nearest.x);
     const newX = nearest.x + Math.cos(angle) * desired;
     const newY = nearest.y + Math.sin(angle) * desired;
