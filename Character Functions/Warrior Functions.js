d803b8b5d449023f0f0591c932fb9ad3590e7c2f
Aegis-940 <jay.sumner996@gmail.com>
2026-03-11 04:29:18 +0000

    1

--- Character Functions/Warrior Functions.js
+++ Character Functions/Warrior Functions.js
@@ -3,8 +3,8 @@
 // CONFIGURATION - Toggle features here instead of editing code
 // ============================================================================
 
-const home = 'targetron';
-const mob_map = 'uhills';
+const home = 'dryad';
+const mob_map = 'mforest';
 const all_bosses = ['grinch', 'icegolem', 'dragold', 'mrgreen', 'mrpumpkin', 'greenjr', 'jr', 'franky', 'rgoo', 'bgoo'];
 
 const CONFIG = {
