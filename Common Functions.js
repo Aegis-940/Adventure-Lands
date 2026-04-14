e2e6ef2772d99dd4c92ee4ed555ec42479729f57
Aegis-940 <jay.sumner996@gmail.com>
2026-04-14 10:56:05 +0000

    1

--- Common Functions.js
+++ Common Functions.js
@@ -52,9 +52,9 @@ const locations = {
 	xscorpion:  [{ x: -495, y: 685 }],
 };
 
-const HEALER_TARGET    = 'mechagnome';
-const WARRIOR_TARGET   = 'mechagnome';
-const RANGER_TARGET    = 'mechagnome';
+const HEALER_TARGET    = 'dryad';
+const WARRIOR_TARGET   = 'dryad';
+const RANGER_TARGET    = 'dryad';
 
 const MERCHANT_TARGET  = { map: "main", x: -87, y: -96 };
 
