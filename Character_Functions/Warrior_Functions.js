20da27a0d80c1b851e0d656d269c916750ee30d1
Aegis-940 <jay.sumner996@gmail.com>
2026-09-10 18:26:34 +1000

    1

--- Character_Functions/Warrior_Functions.js
+++ Character_Functions/Warrior_Functions.js
@@ -11,7 +11,7 @@ var CONFIG = {
 		target_priority: ["Myras"],
 		all_bosses,
 		cleave_min_mobs: 3,
-		cleave_blacklist: ["fireroamer", "plantoid"],
+		cleave_blacklist: ["fireroamer"],
 		agitate_min_mobs: 2,
 		cluster_min_mobs: 2,
 		agitate_blacklist: [],
